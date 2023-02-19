924ca449fa25e66844116fea0801e2537306c323
fabio.matos999 <fabio.matos@upr.edu>
2023-02-19 15:00:06 -0400

    Finished Need and CUrrent Situation

--- Proposal/Main-Document/.auctex-auto/main-document.el
+++ Proposal/Main-Document/.auctex-auto/main-document.el
@@ -3,17 +3,23 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("report" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("hyperref" "hidelinks")))
    (TeX-run-style-hooks
     "latex2e"
     "../Title-Page/title-page"
     "../Informative-Part/informative-part"
     "../Descriptive-Part/descriptive-part"
     "../Analytic-Part/analytic-part"
+    "../References/references"
     "report"
     "rep12"
+    "apacite"
     "unicode"
     "setspace"
     "float"
-    "graphicx"))
+    "graphicx"
+    "hyperref"
+    "natbib"))
  :latex)
 
