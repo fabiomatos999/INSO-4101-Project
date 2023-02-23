f069a22ed4442c5d02871d697bd0d920f31431b9
fabio.matos999 <fabio.matos@upr.edu>
2023-02-23 10:23:01 -0400

    Cleaned working tree and finished scope/span/synop

--- Proposal/Main-Document/.auctex-auto/main-document.el
+++ Proposal/Main-Document/.auctex-auto/main-document.el
@@ -5,6 +5,14 @@
                      '(("report" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("hyperref" "hidelinks")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "../Title-Page/title-page"
@@ -15,11 +23,11 @@
     "report"
     "rep12"
     "apacite"
+    "cite"
     "unicode"
     "setspace"
     "float"
     "graphicx"
-    "hyperref"
-    "natbib"))
+    "hyperref"))
  :latex)
 
