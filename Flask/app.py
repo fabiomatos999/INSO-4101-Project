9ba5b89fa4b5085869b8f264cefaa7b28c4ef3f5
fabiomatos999 <fabio.matos@upr.edu>
2023-03-21 15:13:47 -0400

    Started database implementation

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
